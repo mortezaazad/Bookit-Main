fa82876839cc9f9034b8cd2c885b5eaf5c32cb7e
Morteza Azad <morteza.azad@outlook.com>
2023-04-27 17:27:36 +0430

    Add project files.

---
+++ Bookit-Main/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Bookit_Main.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Bookit_Main.Areas.Identity.IdentityHostingStartup))]
+namespace Bookit_Main.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
---
+++ Bookit-Main/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Bookit_Main.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
