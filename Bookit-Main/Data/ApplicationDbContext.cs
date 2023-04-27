b342eb88f6b4630cb1fed65bd30903e19c8e1f70
Morteza Azad <morteza.azad@outlook.com>
2023-04-27 18:17:33 +0430

    BooksModel

--- Bookit-Main/Data/ApplicationDbContext.cs
+++ Bookit-Main/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Bookit_Main.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -12,5 +13,7 @@ namespace Bookit_Main.Data
             : base(options)
         {
         }
+
+        public DbSet<Book> Books{ get; set; }
     }
 }
