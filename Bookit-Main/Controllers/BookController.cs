bdb9750ebf9b4c93e547ad15c697b042bd7e5075
Morteza Azad <morteza.azad@outlook.com>
2023-04-27 18:45:43 +0430

    BooksCreate

---
+++ Bookit-Main/Controllers/BookController.cs
@@ -0,0 +1,37 @@
+﻿using Bookit_Main.Data;
+using Bookit_Main.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookit_Main.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public BookController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var model= _db.Books;
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Book model)
+        {
+            _db.Books.Add(model);
+            _db.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
